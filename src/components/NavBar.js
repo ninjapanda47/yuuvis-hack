16bcc4c472d0de6af0c8faa9614e24e46939b381
Sandi <sandi.c.gau@gmail.com>
2019-08-24 15:09:36 -0500

    finish search bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,17 +14,18 @@ class NavBar extends Component {
                         <Nav className="mr-auto">
                             <NavLink className="nav-link" to="/">Home</NavLink>
                             <NavLink className="nav-link" to="/upload">Upload</NavLink>
+                            <NavLink className="nav-link" to="/search">Search</NavLink>
                         </Nav>
                     </Navbar.Collapse>
-                    <Navbar.Collapse className="justify-content-end">
-                        <Nav className="justify-content-end">
-                            <NavLink to="/"><Button variant="primary">Login</Button></NavLink>
-                        </Nav>
-                    </Navbar.Collapse>
+                        <Navbar.Collapse className="justify-content-end">
+                            <Nav className="justify-content-end">
+                                <NavLink to="/"><Button variant="primary">Login</Button></NavLink>
+                            </Nav>
+                        </Navbar.Collapse>
                 </Navbar>
             </div>
-        )
-    }
-}
-
+                )
+            }
+        }
+        
 export default NavBar
